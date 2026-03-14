dd7be96102dc99b34a2507ac1cd6139bb9f9443c
Logan-GithubDesktop <akhilthandu13@gmail.com>
2026-03-14 13:13:32 -0500

    Add root redirect to login, fix middleware

--- frontend/middleware.ts
+++ frontend/middleware.ts
@@ -1,6 +1,12 @@
 import { NextRequest, NextResponse } from "next/server";
 
 export function middleware(request: NextRequest) {
+  const { pathname } = request.nextUrl;
+
+  if (pathname === "/") {
+    return NextResponse.redirect(new URL("/login", request.url));
+  }
+
   return NextResponse.next();
 }
 
